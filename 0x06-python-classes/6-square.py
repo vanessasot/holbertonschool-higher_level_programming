13c7fbd5c45275a59d99ef90acd5de3c7c2b9b16
Vanessa <vannexxaa@gmail.com>
2020-09-23 17:14:33 +0000

    Write a class Square that defines a square by: (based on 5-square.py)

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -33,11 +33,11 @@ class Square:
     @position.setter
     def position(self, value):
         """Create setter"""
-        if type(value) is not tuple or len(value) != 2:
+        if type(value) is not tuple or len(value) is not 2:
             raise TypeError("position must be a tuple of 2 positive integers")
-        if type(value[0]) is not int or type(value[1]) is not int:
+        if type(value[0]) is not int or value[0] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
-        if value[0] < 0 or value[1] < 0:
+        if type(value[1]) is not int or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
