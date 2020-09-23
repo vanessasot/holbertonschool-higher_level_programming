f7d5e160de402d5f2c1b869b36a53330c52ed281
Vanessa <vannexxaa@gmail.com>
2020-09-23 17:23:20 +0000

    Write a class Square that defines a square by: (based on 5-square.py)

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -16,12 +16,12 @@ class Square:
         return self.__size
 
     @size.setter
-    def size(self, size):
+    def size(self, value):
         """Create setter"""
 
-        if type(size) is not int:
+        if type(value) is not int:
             raise TypeError("size must be an integer")
-        if size < 0:
+        if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
 
