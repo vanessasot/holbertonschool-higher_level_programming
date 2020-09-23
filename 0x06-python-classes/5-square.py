21e5af4f9cb67d3036397e7d4fda4586ab80ba43
Vanessa <vannexxaa@gmail.com>
2020-09-23 02:17:46 +0000

    Write a class that defines a square by: (based on )

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -32,7 +32,7 @@ class Square:
     def my_print(self):
         """Create my_print method"""
         if self.__size == 0:
-            print(end="")
+            print("")
         for i in range(self.__size):
             for j in range(self.__size):
                 print("#", end="")
