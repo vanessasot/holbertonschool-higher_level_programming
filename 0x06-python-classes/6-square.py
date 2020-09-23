f9d8055b984dd49ba7bb9aa1aa13b42f0bd0567e
Vanessa <vannexxaa@gmail.com>
2020-09-23 17:24:34 +0000

    Write a class Square that defines a square by: (based on 5-square.py)

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -23,7 +23,7 @@ class Square:
             raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
+        self.__size = value
 
     @property
     def position(self):
