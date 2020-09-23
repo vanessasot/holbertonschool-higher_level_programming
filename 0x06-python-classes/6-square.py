6f154e898ae94d0c3b6a1158c3e0662ac0a376f7
Vanessa <vannexxaa@gmail.com>
2020-09-23 17:01:47 +0000

    Write a class Square that defines a square by: (based on 5-square.py)

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+"""Module of square size."""
+
+
+class Square:
+    """Class square size."""
+
+    def __init__(self, size=0, position=(0, 0)):
+        """Initialize the instance."""
+        self.__size = size
+        self.__position = position
+
+    @property
+    def size(self):
+        """Create a property"""
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        """Create setter"""
+
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    @property
+    def position(self):
+        """Create a property"""
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """Create setter"""
+        if type(value) is not tuple or len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if type(value[0]) is not int or type(value[1]) is not int:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if value[0] < 0 or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def area(self):
+        """Create area method"""
+        area = self.__size * self.__size
+        return area
+
+    def my_print(self):
+        """Create my_print method"""
+        if self.__size == 0:
+            print("")
+        for i in range(self.__position[1]):
+            print("")
+        for i in range(self.__size):
+            for j in range(self.__position[0]):
+                print(" ", end="")
+            for k in range(self.__size):
+                print("#", end="")
+            print("")
