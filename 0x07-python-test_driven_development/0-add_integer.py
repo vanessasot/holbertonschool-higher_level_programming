1a0cc2d221ded77a21213abee9909d8b882a30ac
Vanessa <vannexxaa@gmail.com>
2020-09-26 04:59:59 +0000

    F

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -5,9 +5,9 @@
 def add_integer(a, b=98):
     """Instructions of de function"""
     if type(a) is float:
-        a = int (a)
+        a = int(a)
     if type(b) is float:
-        b = int (b)
+        b = int(b)
     if type(a) is not int:
         raise TypeError("a must be an integer")
     if type(b) is not int:
