5c4fdad5808bbcef5f45fb196d4c8893ecd65d93
Vanessa <vannexxaa@gmail.com>
2020-09-29 00:15:52 +0000

    Calculate area and perimeter of the rectangle

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -47,4 +47,6 @@ class Rectangle:
 
     def perimeter(self):
         """Calculate perimeter of the rectangle"""
+        if self.__width == 0 or self.__height == 0:
+            return 0
         return 2 * (self.__width + self.__height)
