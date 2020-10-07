a94a0361ba959e0d186c61e09a1623c1d440c0d0
Vanessa <vannexxaa@gmail.com>
2020-10-07 04:11:47 +0000

    Function that return a value if is or is not a instance for the class

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Create the module"""
+
+
+def is_kind_of_class(obj, a_class):
+    """Define the module"""
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
