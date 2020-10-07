cd15e155c50614b1a12b995e2d04dac7c259da7a
Vanessa <vannexxaa@gmail.com>
2020-10-07 04:00:59 +0000

    Function that return a value

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Create a module"""
+
+
+def is_same_class(obj, a_class):
+    """Define the function"""
+    if type(obj) == a_class:
+        return True
+    else:
+        return False
