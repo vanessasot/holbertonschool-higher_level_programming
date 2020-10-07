d6ab8ae26ed7aa35b6bb6bdcdbc10a29d2d66a40
Vanessa <vannexxaa@gmail.com>
2020-10-07 23:05:17 +0000

    Function that returns the number of lines of a text file

--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -4,6 +4,6 @@
 
 def number_of_lines(filename=""):
     """Function that return the number of lines of the text line"""
-    with open("my_file_0.txt", encoding="utf-8") as myfile:
+    with open(filename, encoding="utf-8") as myfile:
             line = len(myfile.readlines())
     return line
