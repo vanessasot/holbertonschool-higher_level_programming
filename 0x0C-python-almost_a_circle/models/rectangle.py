a801d9c7e10d2660945812155a70890dfccb22c5
Vanessa <vannexxaa@gmail.com>
2020-10-13 21:16:05 +0000

    Add method print

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -71,3 +71,12 @@ class Rectangle(Base):
     def area(self):
         """Calculate area the rectangle"""
         return self.__width * self.__height
+
+    def display(self):
+        """Print the rectangle"""
+        if self.__width == 0 or self.__height == 0:
+            print("")
+        for i in range(self.__height):
+            for j in range(self.__width):
+                print("#", end="")
+            print("")
