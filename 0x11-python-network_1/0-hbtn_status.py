a16e4a9d375a5fb7d23f64f07384989756fdb524
Vanessa <vannexxaa@gmail.com>
2021-01-12 00:50:13 +0000

    fetches https://intranet.hbtn.io/status

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Fetches https://intranet.hbtn.io/status"""
+
+import urllib.request
+
+
+if __name__ == "__main__":
+    req = urllib.request.Request('https://intranet.hbtn.io/status')
+    with urllib.request.urlopen(req) as response:
+        the_page = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(the_page)))
+        print("\t- content: {}".format(the_page))
+        print("\t- utf content: {}".format(the_page.decode('utf-8')))
