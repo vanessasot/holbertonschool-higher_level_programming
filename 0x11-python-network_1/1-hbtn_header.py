15fc983e0eaf7c337b8846167aeb723b4445eacb
Vanessa <vannexxaa@gmail.com>
2021-01-12 04:08:54 +0000

    takes in a URL, sends a request to the URL and displays the value of the X-Request-Id variable found in the header of the response

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Takes in a URL, sends a request to the URL and displays the
+   value of the X-Request-Id variable found in the header of the response """
+
+import urllib.request
+import sys
+
+if __name__ == "__main__":
+    req = urllib.request.Request(sys.argv[1])
+    with urllib.request.urlopen(req) as response:
+        the_page = response.headers['X-Request-Id']
+        print(the_page)
