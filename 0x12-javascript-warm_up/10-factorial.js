00de3ddf32dc4ff6f23821c010808553816b0089
Vanessa <vannexxaa@gmail.com>
2021-02-04 01:13:47 +0000

    Compute and prints a factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// Computes and prints a factorial
+function factorial (n) {
+  if (isNaN(n) || n === 1) {
+    return 1;
+  }
+  return n * factorial(n - 1);
+}
+console.log(factorial(parseInt(process.argv[2])));
