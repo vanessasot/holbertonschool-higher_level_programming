4a9f0f65c821a2146ffcee38d975d5d569943612
Vanessa <vannexxaa@gmail.com>
2021-02-10 01:54:06 +0000

    Display the status code of a GET request

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// Display the status code of a GET request
+
+const request = require('request');
+request
+  .get(process.argv[2])
+  .on('response', function (response) {
+    console.log('code:', response.statusCode);
+  });
